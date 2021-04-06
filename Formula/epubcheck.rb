b93c49e2192b7d33a3baa7e199a0167b984baf74
Sean Molenaar <SMillerDev@users.noreply.github.com>
2021-04-06 15:18:07 +0200

    epubcheck: depend on Java

--- Formula/epubcheck.rb
+++ Formula/epubcheck.rb
@@ -4,6 +4,8 @@ class Epubcheck < Formula
   url "https://github.com/w3c/epubcheck/releases/download/v4.2.5/epubcheck-4.2.5.zip"
   sha256 "2f725b7f491bd7e215fe3fb5492d0554207c838c35165cc195963ddcde39c887"
   license "BSD-3-Clause"
+  
+  depends_on "openjdk"
 
   bottle :unneeded
 
